6d75baf72eead8bdbcc204715a27bf3e85af7c86
LukeFrandsen <lukefrandsen02@gmail.com>
2024-06-10 15:40:09 -0600

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine($"What is your Grade percentage?");
-        string grade = Console.ReadLine();
-        Console.WriteLine("Hello Prep2 World!");
+        float userGrade = float.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (userGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (userGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (userGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (userGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You got a {letter}");
+        if (userGrade >= 70)
+        {
+            Console.WriteLine($"You passed the class");
+        }
+        else
+        {
+            Console.WriteLine($"You failed the class");
+        }
+        
     }
 }
