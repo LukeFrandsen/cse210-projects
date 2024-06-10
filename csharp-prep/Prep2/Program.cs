afb5ae87c55029f27d49177d26da9eebd96d8205
LukeFrandsen <lukefrandsen02@gmail.com>
2024-06-10 14:47:37 -0600

    message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine($"What is your Grade percentage?");
+        string grade = Console.ReadLine();
         Console.WriteLine("Hello Prep2 World!");
     }
 }
