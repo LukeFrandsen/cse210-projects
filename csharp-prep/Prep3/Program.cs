8ebe4833748d2f425482d0abb5d98d38b83e49de
LukeFrandsen <lukefrandsen02@gmail.com>
2024-06-11 14:49:31 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        // Console.WriteLine("What is the magic number?");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        Console.WriteLine("What is your guess for the magic number?");
+        int magicNumerGuess = int.Parse(Console.ReadLine());
+        while (magicNumber != magicNumerGuess)
+        { 
+            if (magicNumerGuess > magicNumber)
+        {
+            Console.WriteLine("Your guess is too high");
+        }
+        else if (magicNumerGuess < magicNumber)
+        {
+            Console.WriteLine("Your guess is too low");
+        }
+            Console.WriteLine("What is your new guess for the magic number?");
+            magicNumerGuess = int.Parse(Console.ReadLine());
+            if (magicNumber == magicNumerGuess)
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+
+        }
+        
     }
 }
