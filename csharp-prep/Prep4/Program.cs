df22532326ad2f62917d86d45938be46e828fa8c
LukeFrandsen <lukefrandsen02@gmail.com>
2024-06-11 15:03:45 -0600

    prep4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber = 1;
+        while (newNumber != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+        }
+
+        Console.WriteLine($"The sum of the numbers is {numbers.Sum()}");
+        Console.WriteLine($"The average of the numbers is {numbers.Average()}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
     }
 }
